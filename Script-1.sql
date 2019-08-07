1a1b170045f95c4c552aa725fdb0a9dac155bf64
Matias Zeitune <matias.zeitune@despegar.com>
2019-08-07 20:02:12 -0300

    postgres script added

---
+++ Script-1.sql
@@ -0,0 +1,80 @@
+CREATE TABLE mezcla (
+    idMezcla SERIAL primary key,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT
+) 
+
+CREATE TABLE cultivo (
+    idCultivo SERIAL primary key,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT
+) 
+
+CREATE TABLE prueba (
+    idPrueba SERIAL primary key,
+    idEnsayo int,
+    idCultivo int,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT
+) 
+
+
+CREATE TABLE ensayo (
+    idEnsayo SERIAL primary key,
+    idUserCreador int,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT
+) 
+
+
+CREATE TABLE mezclaAgroquimico (
+    idMezclaAgroquimico SERIAL primary key,
+    idMezcla int,
+    idAgroquimico int,
+    porcentaje float
+) 
+
+CREATE TABLE agroquimico (
+    idAgroquimico SERIAL primary key,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT
+) 
+
+CREATE TABLE usuario (
+    idUsuario SERIAL primary key,
+    idCompania int,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    usuario VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    creado timestamp,
+    actualizado timestamp,
+    enabled bool
+)
+
+CREATE TABLE usuarioRoles (
+    idUsuarioRol SERIAL primary key,
+    idUsuario int,
+    idRol int
+) 
+
+CREATE TABLE rol (
+    idRol SERIAL primary key,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion text,
+    creado timestamp,
+    actualizado timestamp,
+    enabled bool
+) 
+
+create table compania (
+	idCompania SERIAL primary key,
+	nombre varchar(255) not null,
+	descripcion text
+)
+
+
+
+
+
+
