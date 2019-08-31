de88d7c889df5aa3873c9957d4d30170d86fc22f
Matias Zeitune <matias.zeitune@despegar.com>
2019-08-30 23:10:54 -0300

    postgress changes

--- Script-1.sql
+++ Script-1.sql
@@ -10,10 +10,10 @@ CREATE TABLE cultivo (
     descripcion TEXT
 ) 
 
-CREATE TABLE prueba (
-    idPrueba SERIAL primary key,
+CREATE TABLE experimento (
+    idExperimento SERIAL primary key,
     idEnsayo int,
-    idCultivo int,
+    idMezcla int,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT
 ) 
@@ -22,6 +22,7 @@ CREATE TABLE prueba (
 CREATE TABLE ensayo (
     idEnsayo SERIAL primary key,
     idUserCreador int,
+    idCultivo int,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT
 ) 
@@ -73,13 +74,16 @@ create table compania (
 	descripcion text
 )
 
-alter table prueba add foreign key (idEnsayo) references ensayo(idEnsayo)
+alter table experimento add foreign key (idEnsayo) references ensayo(idEnsayo)
+alter table experimento add foreign key (idMezcla) references mezcla(idMezcla)
 alter table prueba add foreign key (idCultivo) references cultivo(idCultivo)
 alter table ensayo add foreign key (idUserCreador) references usuario(idUsuario)
+alter table ensayo add foreign key (idCultivo) references cultivo(idCultivo)
 alter table mezclaAgroquimico add foreign key (idMezcla) references mezcla(idMezcla)
 alter table mezclaAgroquimico add foreign key (idAgroquimico) references agroquimico(idAgroquimico)
 alter table usuarioRoles add foreign key (idUsuario) references usuario(idUsuario)
 alter table usuarioRoles add foreign key (idRol) references rol(idRol)
+
 
 create table medidas (--tabla provisoria para pruebas mientras no este cassandra productivo
 	idMedida SERIAL primary key,
@@ -88,3 +92,12 @@ create table medidas (--tabla provisoria para pruebas mientras no este cassandra
 	fecha timestamp,
 	medidas text
 )
+
+
+
+insert into cultivo (idCultivo, nombre, descripcion) values (default,'prueba','prueba');
+insert into mezcla (idMezcla, nombre, descripcion) values (default,'prueba','prueba');
+insert into experimento values (default,1,'prueba','prueba');
+insert into usuario values(default,1,'prueba','prueba','prueba','prueba',current_timestamp,current_timestamp,true)
+insert into prueba values (default,1,1,'prueba','prueba');
+
